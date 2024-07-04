cb0d539dd743d8786c29cf72817481e63b72b81d
riwon <137711369+cherry-p0p@users.noreply.github.com>
2024-07-05 06:48:51 +0900

    파일수정

--- Frontend/MyAppDelegate.swift
+++ Frontend/MyAppDelegate.swift
@@ -1,13 +1,11 @@
 import Foundation
 import UIKit
-import KakaoSDKCommon
-import KakaoSDKAuth
-import KakaoSDKUser
+
 
 class MyAppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        KakaoSDK.initSDK(appKey: "45af461a16f86a3fa26a654fc50e4c78")
+
         
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
             if granted {
@@ -19,11 +17,5 @@ class MyAppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
-    
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        if AuthApi.isKakaoTalkLoginUrl(url) {
-            return AuthController.handleOpenUrl(url: url)
-        }
-        return false
-    }
+
 }
