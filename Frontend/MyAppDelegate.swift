2beb59bd1e57f14e87c01367d44b8ba8228d86c3
leeseulgi0208 <106726904+leeseulgi0208@users.noreply.github.com>
2024-07-05 02:24:11 +0900

    local

--- Frontend/MyAppDelegate.swift
+++ Frontend/MyAppDelegate.swift
@@ -8,6 +8,15 @@ class MyAppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         KakaoSDK.initSDK(appKey: "45af461a16f86a3fa26a654fc50e4c78")
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if granted {
+                print("알림 권한 허용")
+            } else {
+                print("알림 권한 거부")
+            }
+        }
+        
         return true
     }
     
